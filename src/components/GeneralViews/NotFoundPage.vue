2e002e2da49b721987b964506687cb785ed5058e
-A- <1157988103@qq.com>
2017-12-30 10:48:57 +0800

    Vcg: Update live demo

--- src/components/GeneralViews/NotFoundPage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    404 no found
-  </div>
-</template>
--- src/globalDirectives.js
+++
@@ -1,7 +0,0 @@
-import { directive as onClickaway } from 'vue-clickaway'
-
-export default {
-  install (Vue) {
-    Vue.directive('click-outside', onClickaway)
-  }
-}
